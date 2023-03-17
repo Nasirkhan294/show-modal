0be925ef8596c0231ed3961a844d89831de079d2
Nasirkhan294 <nasirbhatti.nk@gmail.com>
2023-03-17 17:34:24 +0500

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,13 @@
+let button = document.querySelector('button');
+let modal = document.querySelector(".modal");
+let modalX = document.querySelector('.modal-x')
+
+button.addEventListener('click', function () {
+    modal.classList.add('show-modal');
+    button.innerText = "You've got this!!!"
+});
+
+modalX.addEventListener('click', function () {
+    modal.classList.remove('show-modal');
+    button.innerText = "Who's got this?"    
+})
